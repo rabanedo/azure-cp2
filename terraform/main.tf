1be052b2b3ce28c3f16dc0310b3267f797d2e1cf
rabanedo <122230847+rabanedo@users.noreply.github.com>
2023-02-13 20:54:19 +0100

    Definir los provider
    Definimos los providers en el main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,17 @@
+# INFO: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.43.0"
+    }
+  }
+}
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
